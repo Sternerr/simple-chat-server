d760a909843c4fa22c091483092806bd9198b845
Noah <volpy2000@gmail.com>
2025-07-17 21:06:05 +0200

    added server and client command

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/sternerr/termtalk/internal/server"
 )
 
 func main() {
-	srv, err := server.NewServer("0.0.0.0", "6969")
-	if err != nil {
-		log.Fatal(err.Error())
+	cmd := os.Args[1]
+	switch cmd {
+	case "server":
+		srv, err := server.NewServer("0.0.0.0", "6969")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		srv.Listen()
+		break
+	
+	case "client":
+
+	default:
+		log.Fatalf("unkonw command: %s", cmd)
 	}
-	srv.Listen()
 }
