0d1e8a533abb6c208fff9a4838aa9ab4df309183
Noah <volpy2000@gmail.com>
2025-07-18 10:57:12 +0200

    added a logger

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"log"
 )
 
 func main() {
-	fmt.Println("I will get the job done!")
+	logger := getLogger("/tmp/termtalk_log.txt")
+	(*logger).Println("I will get the job done!")
+}
+
+func getLogger(filename string) *log.Logger {
+	logfile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("could not open file")
+	}
+
+	return log.New(logfile, "[termtalk]", log.Ldate|log.Ltime|log.Lshortfile)
 }
