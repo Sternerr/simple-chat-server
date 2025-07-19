0a669d8d7d3c207203a8975209dd49bd42c6097c
Noah <volpy2000@gmail.com>
2025-07-19 06:31:58 +0200

    added startup cli command

--- main.go
+++ main.go
@@ -3,11 +3,31 @@ package main
 import (
 	"os"
 	"log"
+	"fmt"
 )
 
 func main() {
 	logger := getLogger("/tmp/termtalk_log.txt")
 	(*logger).Println("I will get the job done!")
+
+	if len(os.Args) <= 1 {
+		(*logger).Println("no cli command provided")
+		fmt.Println("please provide a cli command")
+		os.Exit(0)
+	}
+
+
+	cmd := os.Args[1]
+	switch cmd {
+	case "server":
+		(*logger).Println("started server")
+	case "client":
+		(*logger).Println("started client")
+	default:
+		(*logger).Println("invalid command")
+		fmt.Println("invalid command")
+		os.Exit(0)
+	}
 }
 
 func getLogger(filename string) *log.Logger {
